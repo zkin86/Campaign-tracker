87dc7c39bc605f31e737fb64ecd5f4fdfdd7d996
Terho Uotila <terho.uotila@helsinki.fi>
2017-10-30 22:15:17 +0200

    fix tietokantayhteys css+js

--- lib/ConnectionTest/connection_test.php
+++ lib/ConnectionTest/connection_test.php
@@ -148,9 +148,9 @@ try {
   <meta charset="utf-8">
   <title>Tietokantayhteystesti</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
-  <script src="vendor/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
   <style>.panel h2 {text-transform: capitalize; } th {text-transform: capitalize;}</style>
 </head>
 <body style="padding: 25px;">
