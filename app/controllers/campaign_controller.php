412b6e8310527420e830c1cdcff1106be4f9c7b0
zkin86 <eerikki.lehtinen@gmail.com>
2017-12-02 18:46:26 +0200

    oikeiden osoitteiden korjailua

--- app/controllers/campaign_controller.php
+++ app/controllers/campaign_controller.php
@@ -4,7 +4,7 @@ class CampaignController extends BaseController{
 
   public static function index(){
     if(!isset($_SESSION['user'])){
-      Redirect::to('/login', array('error' => 'Kirjaudu ensin sisään!'));
+      Redirect::to('/kirjautuminen', array('error' => 'Kirjaudu ensin sisään!'));
     } else {$campaigns = Campaign::all_for_user(self::get_user_logged_in()->id);
     }
     $campaigns = Campaign::all_for_user(self::get_user_logged_in()->id);
@@ -13,7 +13,7 @@ class CampaignController extends BaseController{
 
   public static function store(){
     if(!isset($_SESSION['user'])){
-      Redirect::to('/login', array('error' => 'Kirjaudu ensin sisään!'));
+      Redirect::to('/kirjautuminen', array('error' => 'Kirjaudu ensin sisään!'));
     } else {$omistaja = self::get_user_logged_in()->id;
     }
 	  $params = $_POST;
@@ -32,14 +32,14 @@ class CampaignController extends BaseController{
 
   public static function new(){
     if(!isset($_SESSION['user'])){
-      Redirect::to('/login', array('error' => 'Kirjaudu ensin sisään!'));
+      Redirect::to('/kirjautuminen', array('error' => 'Kirjaudu ensin sisään!'));
     }
   	View::make('campaign/new.html');
   }
 
   public static function edit($id){
     if(!isset($_SESSION['user'])){
-      Redirect::to('/login', array('error' => 'Kirjaudu ensin sisään!'));
+      Redirect::to('/kirjautuminen', array('error' => 'Kirjaudu ensin sisään!'));
     }
     $campaign = Campaign::find($id);
     View::make('campaign/edit.html', array('campaign' => $campaign));
@@ -47,7 +47,7 @@ class CampaignController extends BaseController{
 
   public static function info($id){
     if(!isset($_SESSION['user'])){
-      Redirect::to('/login', array('error' => 'Kirjaudu ensin sisään!'));
+      Redirect::to('/kirjautuminen', array('error' => 'Kirjaudu ensin sisään!'));
     }
     $campaign = Campaign::find($id);
     if(!is_null($campaign)) {
@@ -64,7 +64,7 @@ class CampaignController extends BaseController{
 
   public static function destroy($id) {
     if(!isset($_SESSION['user'])){
-      Redirect::to('/login', array('error' => 'Kirjaudu ensin sisään!'));
+      Redirect::to('/kirjautuminen', array('error' => 'Kirjaudu ensin sisään!'));
     }
     $campaign = Campaign::find($id);
     Campaign::delete($id);
@@ -73,7 +73,7 @@ class CampaignController extends BaseController{
 
   public static function update($id) {
     if(!isset($_SESSION['user'])){
-      Redirect::to('/login', array('error' => 'Kirjaudu ensin sisään!'));
+      Redirect::to('/kirjautuminen', array('error' => 'Kirjaudu ensin sisään!'));
     }
     $params = $_POST;
     $campaign = new Campaign(array(
